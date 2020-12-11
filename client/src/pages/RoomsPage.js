b454431826a38e352346075f51fa56b1aa7d0db4
BigWaffleMonster <djentisgood1010>
2020-12-11 17:06:34 +0300

    added form for roomspage

--- client/src/pages/RoomsPage.js
+++ client/src/pages/RoomsPage.js
@@ -6,16 +6,15 @@ export const RoomsPage = () => {
   const auth = useContext(AuthContext)
   const {request} = useHttp()
   const [guest, setGuest] = useState('')
+  const [DateIn, setDateIn] = useState('')
+  const [DateOut, setDateOut] = useState('')
 
   const pressHandler = async event => {
-    if (event.key === 'Enter') {
       try {
-        const data = await request('api/book/bookroom', 'POST', {guests: guest}, {
+        const data = await request('api/book/bookroom', 'POST', {guests: guest, DateIn, DateOut}, {
           Authorization: `Bearer ${auth.token}`
         })
-        console.log(data)
       } catch (e) {}
-    }
   }
 
   useEffect(() => {
@@ -25,20 +24,57 @@ export const RoomsPage = () => {
   return (
     <div className="row">
       <div className="col s6 offset-s3">
-        <h1>Hello</h1>
         <div className="input-field">
           <input
-            placeholder="guests"
+            placeholder="1, 2 or 3 persons"
             id="guests"
             type="text"
             name="guests"
             value={guest}
             onChange={e => setGuest(e.target.value)}
-            onKeyPress={pressHandler}
+            style={{marginTop: "1rem"}}
           />
-          <label htmlFor="guests">Email</label>
+          <label htmlFor="guests" style={{marginTop: "1rem"}}>Persons</label>
         </div>
       </div>
+
+      <div className="col s6 offset-s3">
+        <div className="input-field">
+          <input
+            placeholder="Input date"
+            id="DateIn"
+            type="text"
+            name="DateIn"
+            value={DateIn}
+            onChange={e => setDateIn(e.target.value)}
+            style={{marginTop: "1rem"}}
+          />
+          <label htmlFor="DateIn" style={{marginTop: "1rem"}}>Check-in</label>
+        </div>
+      </div>
+
+      <div className="col s6 offset-s3">
+        <div className="input-field">
+          <input
+            placeholder="Input date"
+            id="DateOut"
+            type="text"
+            name="DateOut"
+            value={DateOut}
+            onChange={e => setDateOut(e.target.value)}
+            style={{marginTop: "1rem"}}
+          />
+          <label htmlFor="DateOut" style={{marginTop: "1rem"}}>Check-out</label>
+        </div>
+      </div>
+
+      <button 
+        className="btn yellow darken-4"
+        onClick={pressHandler}
+        style={{marginTop: '22rem'}}
+      >
+        Book
+      </button>
     </div>
   )
 }
--- models/Room.js
+++ models/Room.js
@@ -3,7 +3,6 @@ const {Schema, model, Types} = require('mongoose')
 const schema = new Schema({
   DateIn: {type: Date},
   DateOut: {type: Date},
-  RoomType: {type: String},
   guests: {type: Number, default: 1},
   owner: {type: Types.ObjectId, ref: 'User'}
 })
