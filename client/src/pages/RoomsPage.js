198e5e04c92d931fefa2aca6dd3a11d3709cfa2e
BigWaffleMonster <djentisgood1010>
2020-12-12 21:44:54 +0300

    Created input from for RoomsPage

--- client/src/pages/RoomsPage.js
+++ client/src/pages/RoomsPage.js
@@ -19,62 +19,58 @@ export const RoomsPage = () => {
 
   useEffect(() => {
     window.M.updateTextFields()
+    window.M.AutoInit();
   }, [])
+
 
   return (
     <div className="row">
-      <div className="col s6 offset-s3">
-        <div className="input-field">
-          <input
-            placeholder="1, 2 or 3 persons"
-            id="guests"
-            type="text"
-            name="guests"
-            value={guest}
-            onChange={e => setGuest(e.target.value)}
-            style={{marginTop: "1rem"}}
-          />
-          <label htmlFor="guests" style={{marginTop: "1rem"}}>Persons</label>
-        </div>
+
+      <div className="input-field col s12" style={{marginTop: '1rem'}}>
+        <select
+          id='guest'
+          value={guest}
+          onChange={(e => setGuest(e.target.value))}
+          placeholder="hello"
+        >
+          <option value="" disabled selected>How many persons</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3"> 3</option>
+        </select>
+        <label>Persons</label>
       </div>
 
-      <div className="col s6 offset-s3">
-        <div className="input-field">
-          <input
-            placeholder="Input date"
-            id="DateIn"
-            type="text"
-            name="DateIn"
-            value={DateIn}
-            onChange={e => setDateIn(e.target.value)}
-            style={{marginTop: "1rem"}}
-          />
-          <label htmlFor="DateIn" style={{marginTop: "1rem"}}>Check-in</label>
-        </div>
+      <div className="input-field col s6" style={{marginTop: '1rem'}}>
+        <input 
+          id="DateIn"
+          type="date" 
+          placeholder="Check in" 
+          value={DateIn}
+          onChange={e => setDateIn(e.target.value)} 
+        />
+        <label>Check in</label>
       </div>
 
-      <div className="col s6 offset-s3">
-        <div className="input-field">
-          <input
-            placeholder="Input date"
-            id="DateOut"
-            type="text"
-            name="DateOut"
-            value={DateOut}
-            onChange={e => setDateOut(e.target.value)}
-            style={{marginTop: "1rem"}}
-          />
-          <label htmlFor="DateOut" style={{marginTop: "1rem"}}>Check-out</label>
-        </div>
+      <div className="input-field col s6" style={{marginTop: '1rem'}}>
+        <input
+          id="DateOut" 
+          type="date" 
+          placeholder="Check out"
+          value={DateOut}
+          onChange={(e => setDateOut((e.target.value)))} 
+        />
+        <label>Check out</label>
       </div>
 
       <button 
         className="btn yellow darken-4"
         onClick={pressHandler}
-        style={{marginTop: '22rem'}}
+        style={{marginLeft: '.5rem', marginTop: '1rem'}}
       >
         Book
       </button>
+      
     </div>
   )
 }
